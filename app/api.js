f3a1ffb2e96fc0e8f1414ed1c9f560985b095bed
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-23 14:56:19 -0300

    :pencil2: fix: typo

--- app/api.js
+++ app/api.js
@@ -1,6 +1,27 @@
+import { once } from 'node:events'
 import {createServer} from 'node:http'
+import { DEFAULT_USER } from './config.js'
+import { USER_INVALID_ERROR } from './error/index.js'
+
+
+async function loginRoute(request, response){
+  const {user, password} = JSON.parse(await once(request, 'data'))
+
+  if (user !== DEFAULT_USER.user || password !== DEFAULT_USER.password) {
+    response.writeHead(401)
+    response.end(JSON.stringify(USER_INVALID_ERROR))
+    return;
+  }
+
+  response.end('ok')
+}
+
 
 async function handler(request, response) {
+    
+  if(request.url === '/login' && request.method === 'POST') {
+    return loginRoute(request, response)
+  }
   response.end('Hello Wolrd :D')
 } 
 
